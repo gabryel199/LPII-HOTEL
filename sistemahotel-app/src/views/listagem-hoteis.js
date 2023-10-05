fca90210881d757cf8e4d9ebd6a92ff6ec542e40
Felipe Franco <felipejf739@hotmail.com>
2023-10-05 10:02:35 -0300

    rotas e listagem hotel

--- sistemahotel-app/src/views/listagem-hoteis.js
+++ sistemahotel-app/src/views/listagem-hoteis.js
@@ -32,6 +32,91 @@ function ListagemHoteis() {
     
       const [dados, setDados] = React.useState(null);
 
+      async function excluir(id) {
+        let data = JSON.stringify({ id });
+        let url = `${baseURL}/${id}`;
+        console.log(url);
+        await axios
+          .delete(url, data, {
+            headers: { 'Content-Type': 'application/json' },
+          })
+          .then(function (response) {
+            mensagemSucesso(`Usuário excluído com sucesso!`);
+            setDados(
+              dados.filter((dado) => {
+                return dado.id !== id;
+              })
+            );
+          })
+          .catch(function (error) {
+            mensagemErro(`Erro ao excluir o usuário`);
+          });
+      }
+    
+      React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+          setDados(response.data);
+        });
+      }, []);
+    
+      if (!dados) return null;
+    
+      return (
+        <div className='container'>
+          <Card title='Listagem de Usuários'>
+            <div className='row'>
+              <div className='col-lg-12'>
+                <div className='bs-component'>
+                  <button
+                    type='button'
+                    className='btn btn-warning'
+                    onClick={() => cadastrar()}
+                  >
+                    Novo Usuário
+                  </button>
+                  <table className='table table-hover'>
+                    <thead>
+                      <tr>
+                        <th scope='col'>Login</th>
+                        <th scope='col'>CPF</th>
+                        <th scope='col'>Administrador</th>
+                        <th scope='col'>Ações</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dados.map((dado) => (
+                        <tr key={dado.id}>
+                          <td>{dado.login}</td>
+                          <td>{dado.cpf}</td>
+                          <td>{dado.admin ? 'Sim' : 'Não'}</td>
+                          <td>
+                            <Stack spacing={1} padding={0} direction='row'>
+                              <IconButton
+                                aria-label='edit'
+                                onClick={() => editar(dado.id)}
+                              >
+                                <EditIcon />
+                              </IconButton>
+                              <IconButton
+                                aria-label='delete'
+                                onClick={() => excluir(dado.id)}
+                              >
+                                <DeleteIcon />
+                              </IconButton>
+                            </Stack>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>{' '}
+                </div>
+              </div>
+            </div>
+          </Card>
+        </div>
+      );
+    }
 
-}
+
+
 export default ListagemHoteis;
