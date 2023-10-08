e5248584aec8a601842f3cb2c1b65f826dc887ff
Felipe Franco <felipejf739@hotmail.com>
2023-10-08 13:12:38 -0300

    rotas

--- sistemahotel-app/src/rotas.js
+++ sistemahotel-app/src/rotas.js
@@ -4,12 +4,11 @@ import ListagemCliente from './views/listagem-cliente';
 import ListagemFuncionario from './views/listagem-funcionario';
 import ListagemHoteis from './views/listagem-hoteis';
 import ListagemQuartos from './views/listagem-quartos';
+import ListagemProdutos from './views/listagem-produtos';
+import ListagemServicos from './views/listagem-servicos';
 
 import Login from './views/login';
-//import CadastroUsuario from './views/cadastro-usuario';
-//import CadastroCurso from './views/cadastro-curso';
-//import CadastroProfessor from './views/cadastro-professor';
-//import CadastroAluno from './views/cadastro-aluno';
+
 
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
@@ -22,6 +21,8 @@ function Rotas(props) {
         <Route path='/listagem-funcionario' element={<ListagemFuncionario />} />
         <Route path='/listagem-hoteis' element={<ListagemHoteis />} />
         <Route path='/listagem-quartos' element={<ListagemQuartos />} />
+        <Route path='/listagem-quartos' element={<ListagemProdutos />} />
+        <Route path='/listagem-quartos' element={<ListagemServicos />} />
       </Routes>
     </BrowserRouter>
   );
