36aac3ed975b40ca310f03453ce883f2769ab9d1
Felipe Franco <felipejf739@hotmail.com>
2023-10-08 13:18:36 -0300

    form

--- sistemahotel-app/src/views/listagem-quartos.js
+++ sistemahotel-app/src/views/listagem-quartos.js
@@ -1,19 +1,13 @@
 import React from 'react';
-
 import Card from '../components/card';
 import { mensagemSucesso, mensagemErro } from '../components/toastr';
-
 import '../custom.css';
-
 import { useNavigate } from 'react-router-dom';
-
 import Stack from '@mui/material/Stack';
 import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-
 import axios from 'axios';
-
 import { BASE_URL } from '../config/axios';
 
 const baseURL = `${BASE_URL}/tipoquarto`;
