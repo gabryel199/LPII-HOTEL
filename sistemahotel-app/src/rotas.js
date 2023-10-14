3abc6376fc8126af4b91dd78fb0b72985811bd3b
Felipe Franco <felipejf739@hotmail.com>
2023-10-14 14:12:11 -0300

    correções

--- sistemahotel-app/src/rotas.js
+++ sistemahotel-app/src/rotas.js
@@ -7,6 +7,8 @@ import ListagemProdutos from './views/listagem-produtos';
 import ListagemServicos from './views/listagem-servicos';
 import ListagemComodidades from './views/listagem-comodidades';
 import ListagemQuarto from './views/listagem-quarto';
+import ListagemHospedagem from './views/listagem-hospedagen';
+import ListagemReserva from './views/listagem-reserva';
 
 import Login from './views/login';
 
@@ -24,7 +26,9 @@ function Rotas(props) {
         <Route path='/listagem-produtos' element={<ListagemProdutos />} />
         <Route path='/listagem-servicos' element={<ListagemServicos />} />
         <Route path='/listagem-comodidades' element={<ListagemComodidades />} />
-        <Route path='/listagem-quarto' element={<ListagemQuarto />} />        
+        <Route path='/listagem-quarto' element={<ListagemQuarto />} />      
+        <Route path='/listagem-reserva' element={<ListagemReserva />} />
+        <Route path='/listagem-hospedagen' element={<ListagemHospedagem />} />     
       </Routes>
     </BrowserRouter>
   );
