6127807d2618b4f1489e3fa75b39ede6927af91b
Gabriel Monteiro <gabrielmonteiroifsmg@gmail.com>
2023-10-23 01:02:43 -0300

    os 4 do db feito

--- sistemahotel-app/src/views/cadastro-funcionario.js
+++ sistemahotel-app/src/views/cadastro-funcionario.js
@@ -16,12 +16,27 @@ import { BASE_URL } from '../config/axios';
 
 function CadastroFuncionario() {
 
-  const [id, setId] = useState('');
-  const [nome, setNome] = useState('');
-  const [nome1, setNome1] = useState('');
-  const [nome2, setNome2] = useState('');
-  const [nome3, setNome3] = useState('');
-  const [idCoordenador, setIdCoordenador] = useState(0);
+  const [var0, setVar0] = useState('');//cpf
+  const [var1, setVar1] = useState('');//nome
+  const [var2, setVar2] = useState('');//dataN
+  const [var11, setVar11] = useState('');//email
+  const [var12, setVar12] = useState('');//senha1
+  const [var13, setVar13] = useState('');//senha2
+  const [var14, setVar14] = useState('');//tel
+  
+  const [var15, setVar15] = useState('');//cargo
+  const [var16, setVar16] = useState('');//hInicio
+  const [var17, setVar17] = useState('');//hFim
+  const [var18, setVar18] = useState('');//FolgaSemana
+
+  const [var3, setVar3] = useState('');//pais
+  const [var4, setVar4] = useState('');//uf
+  const [var5, setVar5] = useState('');//cidade
+  const [var6, setVar6] = useState('');//cep
+  const [var7, setVar7] = useState('');//num
+  const [var8, setVar8] = useState('');//com
+  const [var9, setVar9] = useState('');//log
+  const [var10, setVar10] = useState('');//bai 
 
   const [dados, setDados] = React.useState([]);
 
@@ -31,46 +46,198 @@ function CadastroFuncionario() {
         <div className='row'>
           <div className='col-lg-12'>
             <div className='bs-component'>
-              <FormGroup label='Nome: *' htmlFor='inputNome'>
+            <FormGroup label='CPF: *' htmlFor='inputCPF'>
+              <input
+                type='number'
+                id='inputCPF'
+                value={var0}
+                className='form-control'
+                name='cpf'
+                onChange={(e) => setVar0(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup label='Nome: *' htmlFor='inputNome'>
                 <input
                   type='text'
                   id='inputNome'
-                  value={nome}
+                  value={var1}
                   className='form-control'
                   name='nome'
-                  onChange={(e) => setNome(e.target.value)}
+                  onChange={(e) => setVar1(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='Descrição: *' htmlFor='inputDescricao'>
+            <FormGroup label='DataNascimento: *' htmlFor='inputDataNascimento'>
+                <input
+                  type='date'
+                  id='inputDataNascimento'
+                  value={var2}
+                  className='form-control'
+                  name='datanascimento'
+                  onChange={(e) => setVar2(e.target.value)}
+                />
+              </FormGroup>
+            <FormGroup label='Email: *' htmlFor='inputEmail'>
+                <input
+                  type='email'
+                  id='inputEmail'
+                  value={var11}
+                  className='form-control'
+                  name='email'
+                  onChange={(e) => setVar11(e.target.value)}
+                />
+              </FormGroup>
+            <FormGroup label='Senha1: *' htmlFor='inputSenha1'>
+                <input
+                  type='password'
+                  id='inputSenha1'
+                  value={var12}
+                  className='form-control'
+                  name='senha1'
+                  onChange={(e) => setVar12(e.target.value)}
+                />
+              </FormGroup>
+            <FormGroup label='Senha2: *' htmlFor='inputSenha2'>
+                <input
+                  type='password'
+                  id='inputSenha2'
+                  value={var13}
+                  className='form-control'
+                  name='senha2'
+                  onChange={(e) => setVar13(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='Telefone: *' htmlFor='inputTelefone'>
+                <input
+                  type='tel'
+                  id='inputTelefone'
+                  value={var14}
+                  className='form-control'
+                  name='telefone'
+                  onChange={(e) => setVar14(e.target.value)}
+                />
+              </FormGroup>
+            <FormGroup label='Cargo: *' htmlFor='inputCargo'>
                 <input
                   type='text'
-                  id='inputDescricao'
-                  value={nome1}
+                  id='inputCargo'
+                  value={var15}
                   className='form-control'
-                  name='descricao'
-                  onChange={(e) => setNome1(e.target.value)}
+                  name='cargo'
+                  onChange={(e) => setVar15(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='Preço: *' htmlFor='inputPreco'>
+            <FormGroup label='HoraInicio: *' htmlFor='inputHoraInicio'>
+                <input
+                  type='time'
+                  id='inputHoraInicio'
+                  value={var16}
+                  className='form-control'
+                  name='horainicio'
+                  onChange={(e) => setVar16(e.target.value)}
+                />
+              </FormGroup>
+            <FormGroup label='HoraFim: *' htmlFor='inputHoraFim'>
+                <input
+                  type='time'
+                  id='inputHoraFim'
+                  value={var17}
+                  className='form-control'
+                  name='horafim'
+                  onChange={(e) => setVar17(e.target.value)}
+                />
+              </FormGroup>
+            <FormGroup label='FolgaSemana: *' htmlFor='inputFolgaSemana'>
                 <input
                   type='text'
-                  id='inputPreco'
-                  value={nome2}
+                  id='inputFolgaSemana'
+                  value={var18}
                   className='form-control'
-                  name='preco'
-                  onChange={(e) => setNome2(e.target.value)}
+                  name='folgasemana'
+                  onChange={(e) => setVar18(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='Categoria: *' htmlFor='inputCategoria'>
+              <FormGroup label='Pais: *' htmlFor='inputPais'>
                 <input
                   type='text'
-                  id='inputCategoria'
-                  value={nome3}
+                  id='inputPais'
+                  value={var3}
                   className='form-control'
-                  name='categoria'
-                  onChange={(e) => setNome3(e.target.value)}
+                  name='pais'
+                  onChange={(e) => setVar3(e.target.value)}
                 />
               </FormGroup>
+              <FormGroup label='UF: *' htmlFor='inputUF'>
+                <input
+                  type='text'
+                  id='inputUF'
+                  value={var4}
+                  className='form-control'
+                  name='UF'
+                  onChange={(e) => setVar4(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='Cidade: *' htmlFor='inputCidade'>
+                <input
+                  type='text'
+                  id='inputCidade'
+                  value={var5}
+                  className='form-control'
+                  name='cidade'
+                  onChange={(e) => setVar5(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='CEP: *' htmlFor='inputCEP'>
+                <input
+                  type='number'
+                  id='inputCEP'
+                  value={var6}
+                  className='form-control'
+                  name='CEP'
+                  onChange={(e) => setVar6(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='nº: *' htmlFor='inputNum'>
+                <input
+                  type='number'
+                  id='inputNum'
+                  value={var7}
+                  className='form-control'
+                  name='num'
+                  onChange={(e) => setVar7(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='Complemento: *' htmlFor='inputComplemento'>
+                <input
+                  type='number'
+                  id='inputComplemento'
+                  value={var8}
+                  className='form-control'
+                  name='complemento'
+                  onChange={(e) => setVar8(e.target.value)}
+                />
+                </FormGroup>
+              <FormGroup label='Logradouro: *' htmlFor='inputLogradouro'>
+                <input
+                  type='text'
+                  id='inputLogradouro'
+                  value={var9}
+                  className='form-control'
+                  name='logradouro'
+                  onChange={(e) => setVar9(e.target.value)}
+                />
+                </FormGroup>
+              <FormGroup label='Bairro: *' htmlFor='inputBairro'>
+                <input
+                  type='text'
+                  id='inputBairro'
+                  value={var10}
+                  className='form-control'
+                  name='bairro'
+                  onChange={(e) => setVar10(e.target.value)}
+                />
+                </FormGroup>
+
+
               <br></br>
               <Stack spacing={1} padding={1} direction='row'>
                 <button
--- sistemahotel-app/src/views/cadastro-hotel.js
+++ sistemahotel-app/src/views/cadastro-hotel.js
@@ -16,12 +16,19 @@ import { BASE_URL } from '../config/axios';
 
 function CadastroHotel() {
 
-  const [id, setId] = useState('');
-  const [nome, setNome] = useState('');
-  const [nome1, setNome1] = useState('');
-  const [nome2, setNome2] = useState('');
-  const [nome3, setNome3] = useState('');
-  const [idCoordenador, setIdCoordenador] = useState(0);
+  const [var0, setVar0] = useState('');
+  const [var1, setVar1] = useState('');
+  const [var2, setVar2] = useState('');
+
+  const [var3, setVar3] = useState('');//pais
+  const [var4, setVar4] = useState('');//uf
+  const [var5, setVar5] = useState('');//cidade
+  const [var6, setVar6] = useState('');//cep
+  const [var7, setVar7] = useState('');//num
+  const [var8, setVar8] = useState('');//com
+  const [var9, setVar9] = useState('');//log
+  const [var10, setVar10] = useState('');//bai 
+                                         
 
   const [dados, setDados] = React.useState([]);
 
@@ -31,46 +38,119 @@ function CadastroHotel() {
         <div className='row'>
           <div className='col-lg-12'>
             <div className='bs-component'>
-              <FormGroup label='Nome: *' htmlFor='inputNome'>
+              <FormGroup label='Titulo: *' htmlFor='inputTitulo'>
                 <input
                   type='text'
-                  id='inputNome'
-                  value={nome}
+                  id='inputTitulo'
+                  value={var0}
                   className='form-control'
-                  name='nome'
-                  onChange={(e) => setNome(e.target.value)}
+                  name='titulo'
+                  onChange={(e) => setVar0(e.target.value)}
                 />
               </FormGroup>
               <FormGroup label='Descrição: *' htmlFor='inputDescricao'>
                 <input
                   type='text'
                   id='inputDescricao'
-                  value={nome1}
+                  value={var1}
                   className='form-control'
                   name='descricao'
-                  onChange={(e) => setNome1(e.target.value)}
+                  onChange={(e) => setVar1(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='Preço: *' htmlFor='inputPreco'>
+              <FormGroup label='Telefone: *' htmlFor='inputTelefone'>
+                <input
+                  type='tel'
+                  id='inputTelefone'
+                  value={var2}
+                  className='form-control'
+                  name='telefone'
+                  onChange={(e) => setVar2(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='Pais: *' htmlFor='inputPais'>
                 <input
                   type='text'
-                  id='inputPreco'
-                  value={nome2}
+                  id='inputPais'
+                  value={var3}
                   className='form-control'
-                  name='preco'
-                  onChange={(e) => setNome2(e.target.value)}
+                  name='pais'
+                  onChange={(e) => setVar3(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='Categoria: *' htmlFor='inputCategoria'>
+              <FormGroup label='UF: *' htmlFor='inputUF'>
                 <input
                   type='text'
-                  id='inputCategoria'
-                  value={nome3}
+                  id='inputUF'
+                  value={var4}
                   className='form-control'
-                  name='categoria'
-                  onChange={(e) => setNome3(e.target.value)}
+                  name='UF'
+                  onChange={(e) => setVar4(e.target.value)}
                 />
               </FormGroup>
+              <FormGroup label='Cidade: *' htmlFor='inputCidade'>
+                <input
+                  type='text'
+                  id='inputCidade'
+                  value={var5}
+                  className='form-control'
+                  name='cidade'
+                  onChange={(e) => setVar5(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='CEP: *' htmlFor='inputCEP'>
+                <input
+                  type='number'
+                  id='inputCEP'
+                  value={var6}
+                  className='form-control'
+                  name='CEP'
+                  onChange={(e) => setVar6(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='nº: *' htmlFor='inputNum'>
+                <input
+                  type='number'
+                  id='inputNum'
+                  value={var7}
+                  className='form-control'
+                  name='num'
+                  onChange={(e) => setVar7(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='Complemento: *' htmlFor='inputComplemento'>
+                <input
+                  type='number'
+                  id='inputComplemento'
+                  value={var8}
+                  className='form-control'
+                  name='complemento'
+                  onChange={(e) => setVar8(e.target.value)}
+                />
+                </FormGroup>
+              <FormGroup label='Logradouro: *' htmlFor='inputLogradouro'>
+                <input
+                  type='text'
+                  id='inputLogradouro'
+                  value={var9}
+                  className='form-control'
+                  name='logradouro'
+                  onChange={(e) => setVar9(e.target.value)}
+                />
+                </FormGroup>
+              <FormGroup label='Bairro: *' htmlFor='inputBairro'>
+                <input
+                  type='text'
+                  id='inputBairro'
+                  value={var10}
+                  className='form-control'
+                  name='bairro'
+                  onChange={(e) => setVar10(e.target.value)}
+                />
+                </FormGroup>
+
+
+
               <br></br>
               <Stack spacing={1} padding={1} direction='row'>
                 <button
