8884b0c8aeab94c59ec5336f588c73ecabc05a4a
Gabriel Monteiro <gabrielmonteiroifsmg@gmail.com>
2023-10-28 22:59:44 -0300

    axio

--- sistemahotel-app/src/config/axios.js
+++ sistemahotel-app/src/config/axios.js
@@ -1,7 +1,13 @@
 export const BASE_URL =
   // 'http://localhost:8080/api/v1';
-  // 'https://my-json-server.typicode.com/gabryel199/LPII-HOTEL'; // produto e tipo , cliente e pais
-  // 'https://my-json-server.typicode.com/gabryel199/LP2-Hotel-2'; //reserva hospedagem hotel funcionario
-  'https://my-json-server.typicode.com/gabryel199/LP2-Hotel-3'; // quarto e tipo, comodidade e tipo
+  'https://my-json-server.typicode.com/gabryel199/LPII-HOTEL'; // produto e tipo , cliente e pais
 
-  export const BASE_URL_endereco = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-endereco';
+  export const URL_comodidade = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-comodidade';
+  export const URL_hotel = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-hotel';
+  export const URL_quarto = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-quarto';
+  export const URL_produto = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-produto';
+  export const URL_endereco = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-endereco';
+  export const URL_funcionario = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-funcionario';
+  export const URL_telefone = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-telefone';
+  export const URL_hospedagem = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-hospedagem';
+  export const URL_servico = 'https://my-json-server.typicode.com/gabryel199/LP2-HOTEL-servico';
