72d2cf25fd6e11cdeb7678ef6b2a2f1c203dc923
gabryel199 <144912479+gabryel199@users.noreply.github.com>
2023-11-23 09:11:46 -0300

    Update cadastro-servicos.js

--- sistemahotel-app/src/views/Cadastros/cadastro-servicos.js
+++ sistemahotel-app/src/views/Cadastros/cadastro-servicos.js
@@ -12,154 +12,163 @@ import { mensagemSucesso, mensagemErro } from '../../components/toastr';
 import '../../custom.css';
 
 import axios from 'axios';
-import { URL_servico } from '../../config/axios';
+import { BASE_URL } from '../../config/axios';
+import { URL_quarto } from '../../config/axios';
 import { URL_hotel } from '../../config/axios';
+import { URL_status } from '../../config/axios';
 
-function CadastroServicos() {
+function CadastroQuarto() {
   
   const { idParam } = useParams();
 
   const navigate = useNavigate();
 
-  const baseURL = `${URL_servico}/servico`;
+  const baseURL = `${URL_quarto}/quarto`;
 
   const [id, setId] = useState(0);
-  const [var0, setVar0] = useState('');//nome
-  const [var1, setVar1] = useState('');//desc
-  const [var2, setVar2] = useState('');//valor h orario
+  const [var0, setVar0] = useState('');//num
+  const [var1, setVar1] = useState('');//andar
+  const [var2, setVar2] = useState('');//bloco
   const [var3, setVar3] = useState('');//status
-  const [var4, setVar4] = useState(false);//tipo reserva
-  const [var5, setVar5] = useState('');//id hotel
-  const [var6, setVar6] = useState('');//id tipo serv
+  const [var4, setVar4] = useState('');//hotelid
+  const [var5, setVar5] = useState('');//tipoquartoid
+  const [var6, setVar6] = useState('');
+  const [var7, setVar7] = useState('');
+  const [var8, setVar8] = useState('');
+  const [var9, setVar9] = useState('');
 
-   //ESSA é A PARTE DO BOTAO EDITAR
-   const [dados, setDados] = React.useState([]);
-  
-   function inicializar() {
-     if (idParam == null) {
-       setId('');
-       setVar0('');
-       setVar1('');
-       setVar2('');
-       setVar3('');
-       setVar4(false);
-       setVar5('');
-     } else {
-       setId(dados.id);
-       setVar0(dados.titulo);
-       setVar1(dados.descricao);
-       setVar2(dados.valorhorario);
-       setVar3(dados.status);
-       setVar4(dados.tipoReserva);
-       setVar5(dados.hotel_id);
-       setVar6(dados.tipoServico_id);
-     }
-   }
- 
-   async function salvar() {
-     let data = {
-       id,
-       var0,
-       var1,
-       var2,
-       var3,
-       var4,
-       var5,
-       var6
-     };
-     data = JSON.stringify(data);
-     if (idParam == null) {
-       await axios
-         .post(baseURL, data, {
-           headers: { 'Content-Type': 'application/json' },
-         })
-         .then(function (response) {
-           mensagemSucesso(`Serviço ${var0} cadastrado com sucesso!`);
-           navigate(`/listagem-servicos`);
-         })
-         .catch(function (error) {
-           mensagemErro(error.response.data);
-         });
-     } else {
-       await axios
-         .put(`${baseURL}/${idParam}`, data, {
-           headers: { 'Content-Type': 'application/json' },
-         })
-         .then(function (response) {
-           mensagemSucesso(`Serviço ${var0} alterado com sucesso!`);
-           navigate(`/listagem-servicos`);
-         })
-         .catch(function (error) {
-           mensagemErro(error.response.data);
-         });
-     }
-   }
- 
-   async function buscar() {
-     if (idParam != null) {
-       await axios.get(`${baseURL}/${idParam}`).then((response) => {
-         setDados(response.data);
-       });
-       setId(dados.id);
-       setVar0(dados.titulo);
-       setVar1(dados.descricao);
-       setVar2(dados.valorhorario);
-       setVar3(dados.status);
-       setVar4(dados.tipoReserva);
-       setVar5(dados.hotel_id);
-       setVar6(dados.tipoServico_id);
-     }
-   }
+  const [dados, setDados] = React.useState([]); // quarto
 
-  const [dados2, setDados2] = React.useState(null); //tipo servcio
+  function inicializar() {
+    if (idParam == null) {
+      setId('');
+      setVar0('');
+      setVar1('');
+      setVar2('');
+      setVar3('');
+      setVar4('');
+      setVar5('');
+    } else {
+      setId(dados.id);
+      setVar0(dados.numero);
+      setVar1(dados.andar);
+      setVar2(dados.bloco);
+      setVar3(dados.status);
+      setVar4(dados.hotel_id);
+      setVar5(dados.tipoQuarto_id);
+    }
+  }
+
+  async function salvar() {
+    let data = {
+      id,
+      var0,
+      var1,
+      var2,
+      var3,
+      var4,
+      var5
+    };
+    data = JSON.stringify(data);
+    if (idParam == null) {
+      await axios
+        .post(baseURL, data, {
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(function (response) {
+          mensagemSucesso(`Quarto ${var0} cadastrado com sucesso!`);
+          navigate(`/listagem-quarto`);
+        })
+        .catch(function (error) {
+          mensagemErro(error.response.data);
+        });
+    } else {
+      await axios
+        .put(`${baseURL}/${idParam}`, data, {
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(function (response) {
+          mensagemSucesso(`Quarto ${var0} alterado com sucesso!`);
+          navigate(`/listagem-quarto`);
+        })
+        .catch(function (error) {
+          mensagemErro(error.response.data);
+        });
+    }
+  }
+
+  async function buscar() {
+    if (idParam != null) {
+      await axios.get(`${baseURL}/${idParam}`).then((response) => {
+        setDados(response.data);
+      });
+      setId(dados.id);
+      setVar0(dados.numero);
+      setVar1(dados.andar);
+      setVar2(dados.bloco);
+      setVar3(dados.status);
+      setVar4(dados.hotel_id);
+      setVar5(dados.tipoQuarto_id);
+    }
+  }
+
+  const [dados2, setDados2] = React.useState(null); //tipo quarto
 
   useEffect(() => {
-    axios.get(`${URL_servico}/tipoServico`).then((response) => {
+    axios.get(`${URL_quarto}/tipoQuarto`).then((response) => {
       setDados2(response.data);
     });
   }, []);
-
-  const [dados3, setDados3] = React.useState(null); //hotel
+  
+  const [dados3, setDados3] = React.useState(null); //tipo quarto
 
   useEffect(() => {
-    //axios.get(`${URL_produto}/tipoProduto`).then((response) => {
     axios.get(`${URL_hotel}/hotel`).then((response) => {
       setDados3(response.data);
     });
   }, []);
 
-  useEffect(() => {
-    buscar(); // eslint-disable-next-line
-  }, [id]);
+  const [dados4, setDados4] = React.useState(null); //tipo Produto
   
+  useEffect(() => {
+    axios.get(`${URL_status}/statusQuarto`).then((response) => {
+      setDados4(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+      buscar(); // eslint-disable-next-line
+  }, [id]);
+
   if (!dados) return null;
   if (!dados2) return null;
   if (!dados3) return null;
+  if (!dados4) return null;
+
 
   return (
     <div className='container'>
-      <Card title='Cadastro de Serviços'>
+      <Card title='Cadastro de Quartos'>
         <div className='row'>
           <div className='col-lg-12'>
             <div className='bs-component'>
-              <FormGroup label='Nome: *' htmlFor='inputNome'>
-                <input
-                  type='text'
-                  id='inputNome'
-                  value={var0}
-                  className='form-control'
-                  name='nome'
-                  onChange={(e) => setVar0(e.target.value)}
-                />
-              </FormGroup>
-              <FormGroup label='Descrição:' htmlFor='inputDescricao'>
-                <textarea
-                  id='inputDescricao'
-                  value={var1}
-                  className='form-control'
-                  name='descricao'
-                  onChange={(e) => setVar1(e.target.value)}
-                />
+              <FormGroup label='Status: *' htmlFor='selectStatus'>
+                <select
+                  className='form-select'
+                  id='selectStatus'
+                  name='status'
+                  value={var3}
+                  onChange={(e) => setVar3(e.target.value)}
+                >
+                  <option key='0' value='0'>
+                    {' '}
+                  </option>
+                  {dados4.map((dado) => (
+                    <option key={dado.id} value={dado.id}>
+                      {dado.titulo}
+                    </option>
+                  ))}
+                </select>
               </FormGroup>
               <FormGroup label='Tipo: *' htmlFor='selectTipo'>
                 <select
@@ -179,61 +188,43 @@ function CadastroServicos() {
                   ))}
                 </select>
               </FormGroup>
-              <FormGroup label='Tipo de reserva: *' htmlFor='inputTipoReserva'>
-                {/* <input 
-                  type="checkbox" 
-                  className="form-check-input" 
-                  id="btncheck1" 
-                  autocomplete="off"
-                  checked={var4}
-                  name='tiporeserva'
-                  onChange={(e) => setVar4(e.target.checked)}>
-                    </input> */}
-
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked={var4} onChange={(e) => setVar4(e.target.checked)}/>
-                        Agendada
-                    </div>
-                    <div class="form-check">
-                      <input class="form-check-input" type="radio" name="optionsRadios" id="optionsRadios2" value="option2" checked={!var4} onChange={(e) => setVar4(!e.target.checked)}/>
-                        Não agendada
-                    </div>
-                {/* <input
-                  type='texte' //checkbox
-                  id='inputTipoReserva'
-                  value={var4}
-                  className='form-control'
-                  name='tiporeserva'
-                  onChange={(e) => setVar4(e.target.checked)}
-                /> */}
-              </FormGroup>
-              <FormGroup label='Preço por horário: *' htmlFor='inputPreco'>
+              <FormGroup label='Número: *' htmlFor='inputNumero'>
                 <input
-                  type='text'
-                  id='inputPreco'
-                  value={var2}
+                  type='number'
+                  id='inputNumero'
+                  value={var0}
                   className='form-control'
-                  name='preco'
-                  onChange={(e) => setVar2(e.target.value)}
+                  name='numero'
+                  onChange={(e) => setVar0(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='Status: *' htmlFor='inputStatus'>
+              <FormGroup label='Andar: *' htmlFor='inputAndar'>
                 <input
-                  type='text'
-                  id='inputStatus'
-                  value={var3}
+                  type='number'
+                  id='inputAndar'
+                  value={var1}
                   className='form-control'
-                  name='status'
-                  onChange={(e) => setVar3(e.target.value)}
+                  name='andar'
+                  onChange={(e) => setVar1(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='Bloco: *' htmlFor='inputBloco'>
+                <input
+                  type='number'
+                  id='inputBloco'
+                  value={var2}
+                  className='form-control'
+                  name='bloco'
+                  onChange={(e) => setVar2(e.target.value)}
                 />
               </FormGroup>
               <FormGroup label='Hotel: *' htmlFor='selectHotel'>
                 <select
                   className='form-select'
                   id='selectHotel'
-                  name='hotel'
-                  value={var6}
-                  onChange={(e) => setVar6(e.target.value)}
+                  name='Hotel'
+                  value={var4}
+                  onChange={(e) => setVar4(e.target.value)}
                 >
                   <option key='0' value='0'>
                     {' '}
@@ -271,4 +262,4 @@ function CadastroServicos() {
   );
 }
 
-export default CadastroServicos;
+export default CadastroQuarto;
