c45e1b61c89be7dd12c4734d10c9ea537643d234
gabryel199 <144912479+gabryel199@users.noreply.github.com>
2024-02-22 08:38:47 -0300

    Add files via upload

---
+++ LPIII/SCHapi/SCHapi/src/test/java/com/example/SCHapi/ScHapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SCHapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScHapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
