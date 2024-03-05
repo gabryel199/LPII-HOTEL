2c4c3c764be5062268a257461b26d685cae2f089
Felipe Franco <felipejf739@hotmail.com>
2024-03-05 08:16:09 -0300

    endereço

--- LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/model/entity/Endereco.java
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/model/entity/Endereco.java
@@ -1,9 +1,6 @@
 package com.example.SCHapi.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,5 +25,12 @@ public class Endereco {
     private String cep;
 
 
+    @ManyToOne
+    private Uf uf;
 
+    @OneToOne
+    private Hotel hotel;
+
+    @OneToOne
+    private Usuario usuario;
 }
