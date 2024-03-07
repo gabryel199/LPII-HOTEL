c86dc19c0169bba31cc9fc7a95dca9a3178af21d
Felipe Franco <felipejf739@hotmail.com>
2024-03-07 08:07:26 -0300

    save

---
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/api/dto/AvaliacaoHospedagemDTO.java
@@ -0,0 +1,10 @@
+package com.example.SCHapi.api.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AvaliacaoHospedagemDTO {
+}
