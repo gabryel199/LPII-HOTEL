23de20706fb357ad6eaefa1bab2e0ff441190eeb
gabryel199 <144912479+gabryel199@users.noreply.github.com>
2024-03-07 08:30:14 -0300

    Update FuncionarioDTO.java

--- LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/api/dto/FuncionarioDTO.java
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/api/dto/FuncionarioDTO.java
@@ -7,4 +7,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class FuncionarioDTO {
+  private Long id;
+  private String nome;
+  private String cpf;
+  private Date dataNacimento;
+  private String email;
+  private String senha;
+  private String salario;
+  private String horaInicio;
+  private String horaFim;
+  private String ddi1;
+  private String ddd1;
+  private String num1;
+  private String ddi2;
+  private String ddd2;
+  private String num2;
+  private String numero;,
+  private String complemento;
+  private String logradouro;
+  private String bairro;
+  private String cep;
+  private String idCidade;
+  private String idUf;
+  private String idPais;
+  private String idHotel;
+  private String idCargo;
 }
