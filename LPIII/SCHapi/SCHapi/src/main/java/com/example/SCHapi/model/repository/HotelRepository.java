955e4037e0357e95e907b6a59ebf7528280c1aa4
Felipe Franco <felipejf739@hotmail.com>
2024-03-12 08:06:54 -0300

    save

--- LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/model/repository/HotelRepository.java
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/model/repository/HotelRepository.java
@@ -1,6 +1,7 @@
 package com.example.SCHapi.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.SCHapi.model.entity.Hotel;
 
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
+public interface HotelRepository extends JpaRepository<com.example.SCHapi.model.entity.Hotel, Long> {
 }
