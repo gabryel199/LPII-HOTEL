9120aa0c3d1dca0fd2521c77709af181a0c8500e
Felipe Franco <felipejf739@hotmail.com>
2024-04-02 08:39:18 -0300

    save

--- LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/model/entity/Hotel.java
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/model/entity/Hotel.java
@@ -19,22 +19,10 @@ public class Hotel {
     private String titulo;
     private String descricao;
     private Float avaliacaoMedia;
-    private Integer ddd1;
-    private Integer num1;
-    private Integer ddi2;
-    private Integer ddd2;
-    private Integer num2;
-    private Integer numero;
-    private String complemento;
-    private String logradouro;
-    private String bairro;
-    private String cep;
-    private String cidade;
+    private Integer telefone1;
+    private Integer telefone2;
 
-    @ManyToOne
-    private Uf uf;
-    @ManyToOne
-    private Pais pais;
+
 
     @OneToOne(cascade = CascadeType.ALL)
     private Endereco endereco;
