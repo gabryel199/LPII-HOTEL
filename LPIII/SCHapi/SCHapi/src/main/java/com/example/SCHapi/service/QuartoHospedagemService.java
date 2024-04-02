9f98aa58e6428e79cde05b1ba952abd4f4f6c12e
Gabriel Monteiro <gabrielmonteiroifsmg@gmail.com>
2024-04-02 12:43:26 -0300

    correcao QuartoHospedagemService

--- LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/service/QuartoHospedagemService.java
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/service/QuartoHospedagemService.java
@@ -3,26 +3,26 @@ package com.example.SCHapi.service;
 import com.example.SCHapi.model.entity.Quarto;
 import com.example.SCHapi.model.entity.QuartoHospedagem;
 import com.example.SCHapi.model.repository.QuartoRepository;
-import com.example.SCHapi.model.repository.QuartosHospedagemRepository;
+import com.example.SCHapi.model.repository.QuartoHospedagemRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class QuartosHospedagemService {
+public class QuartoHospedagemService {
 
-    private QuartosHospedagemRepository repository;
+    private QuartoHospedagemRepository repository;
 
-    public QuartosHospedagemService(QuartosHospedagemRepository repository) {
+    public QuartoHospedagemService(QuartoHospedagemRepository repository) {
         this.repository = repository;
     }
 
-    public List<QuartoHospedagem> getQuartosHospedagem(){
+    public List<QuartoHospedagem> getQuartoHospedagens(){
         return repository.findAll();
     }
 
-    public Optional<QuartoHospedagem> getQuartosHospedagemById(Long id) {
+    public Optional<QuartoHospedagem> getQuartoHospedagemById(Long id) {
         return  repository.findById(id);
     }
 }
