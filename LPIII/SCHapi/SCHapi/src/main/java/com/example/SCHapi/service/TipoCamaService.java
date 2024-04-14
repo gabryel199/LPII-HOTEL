12c1d0089fd1df01213aac40c4321068adadbaf8
Felipe Franco <felipejf739@hotmail.com>
2024-04-14 10:46:00 -0300

    post tipo cama

--- LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/service/TipoCamaService.java
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/service/TipoCamaService.java
@@ -1,10 +1,12 @@
 package com.example.SCHapi.service;
 
+import com.example.SCHapi.exception.RegraNegocioException;
 import com.example.SCHapi.model.entity.StatusHospedagem;
 import com.example.SCHapi.model.entity.TipoCama;
 import com.example.SCHapi.model.repository.StatusHospedagemRepository;
 import com.example.SCHapi.model.repository.TipoCamaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,5 +25,26 @@ public class TipoCamaService {
 
     public Optional<TipoCama> getTipoCamaById(Long id) {
         return repository.findById(id);
+    }
+
+    @Transactional
+    public TipoCama salvar(TipoCama tipoCama) {
+        validar(tipoCama);
+        return repository.save(tipoCama);
+    }
+
+    public void validar(TipoCama tipoCama) {
+
+        Integer ocupantes = tipoCama.getOcupantes();
+
+        if (tipoCama.getTitulo() == null || tipoCama.getTitulo().trim().equals("")){
+            throw new RegraNegocioException("Titulo Invalido!!! Insira um titulo valido.");
+        }
+        if (tipoCama.getDescricao() == null || tipoCama.getDescricao().trim().equals("")) {
+            throw new RegraNegocioException("Descrição Invalida!!! Insira uma descrição valida.");
+        }
+        if (ocupantes <= 0 || ocupantes == null) {
+            throw new RegraNegocioException("A quantidade de ocupupantes em uma cama tem que ser maior que 0.");
+        }
     }
 }
