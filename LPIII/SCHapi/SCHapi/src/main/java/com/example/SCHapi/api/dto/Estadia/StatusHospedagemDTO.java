7f3d9f0c8874c7f3f1f946cd318c20e85ab5eb94
Felipe Franco <felipejf739@hotmail.com>
2024-04-22 10:37:28 -0300

    status hospedagem dto
    criação

---
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/api/dto/Estadia/StatusHospedagemDTO.java
@@ -0,0 +1,25 @@
+package com.example.SCHapi.api.dto.Estadia;
+
+import com.example.SCHapi.model.entity.Estadia.StatusHospedagem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class StatusHospedagemDTO {
+
+    private Long id;
+
+    private String itulo;
+
+    public static StatusHospedagemDTO create(StatusHospedagem statusHospedagem) {
+        ModelMapper modelMapper = new ModelMapper();
+        StatusHospedagemDTO dto = modelMapper.map(statusHospedagem, StatusHospedagemDTO.class);
+
+        return dto;
+    }
+
+}
