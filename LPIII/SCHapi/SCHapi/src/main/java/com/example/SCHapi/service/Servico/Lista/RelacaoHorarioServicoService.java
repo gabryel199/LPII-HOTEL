f0147225a0121a44a53bc96554794d5394625a59
Felipe Franco <felipejf739@hotmail.com>
2024-04-26 09:52:46 -0300

    relação horario serviço
    metodo

--- LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/service/Servico/Lista/RelacaoHorarioServicoService.java
+++ LPIII/SCHapi/SCHapi/src/main/java/com/example/SCHapi/service/Servico/Lista/RelacaoHorarioServicoService.java
@@ -1,9 +1,13 @@
 package com.example.SCHapi.service.Servico.Lista;
 
+import com.example.SCHapi.exception.RegraNegocioException;
+import com.example.SCHapi.model.entity.*;
+import com.example.SCHapi.model.entity.Quarto.Lista.TipoCamaTipoQuarto;
 import com.example.SCHapi.model.entity.Servico.Lista.RelacaoHorarioServico;
 import com.example.SCHapi.model.repository.Servico.Lista.RelacaoHorarioServicoRepository;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,5 +26,11 @@ public class RelacaoHorarioServicoService {
 
     public Optional<RelacaoHorarioServico> getRelacaoHorariosServicoById(Long id) {
         return repository.findById(id);
+    }
+
+    @Transactional
+    public RelacaoHorarioServico salvar(RelacaoHorarioServico relacaoHorarioServico) {
+        // validar(tipoCamaTipoQuarto);
+        return repository.save(relacaoHorarioServico);
     }
 }
